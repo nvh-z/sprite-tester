66dfdfa03a08f627441d07164a00a4da8ab6c4b5
nvh-z <nielsvanhamme35@gmail.com>
2024-10-20 23:23:56 +0200

    Init

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ComboBox1 = new System.Windows.Forms.ComboBox();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.Timer1 = new System.Windows.Forms.Timer(this.components);
+            this.PictureBox1 = new System.Windows.Forms.PictureBox();
+            this.PictureBox3 = new System.Windows.Forms.PictureBox();
+            this.PictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ComboBox1
+            // 
+            this.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ComboBox1.FormattingEnabled = true;
+            this.ComboBox1.Items.AddRange(new object[] {
+            "Up",
+            "Down",
+            "Left",
+            "Right"});
+            this.ComboBox1.Location = new System.Drawing.Point(254, 12);
+            this.ComboBox1.Name = "ComboBox1";
+            this.ComboBox1.Size = new System.Drawing.Size(143, 21);
+            this.ComboBox1.TabIndex = 3;
+            // 
+            // Button1
+            // 
+            this.Button1.Location = new System.Drawing.Point(254, 39);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(143, 29);
+            this.Button1.TabIndex = 2;
+            this.Button1.Text = "Open Sprite";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Timer1
+            // 
+            this.Timer1.Enabled = true;
+            this.Timer1.Interval = 150;
+            this.Timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // PictureBox1
+            // 
+            this.PictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.PictureBox1.Name = "PictureBox1";
+            this.PictureBox1.Size = new System.Drawing.Size(235, 166);
+            this.PictureBox1.TabIndex = 0;
+            this.PictureBox1.TabStop = false;
+            // 
+            // PictureBox3
+            // 
+            this.PictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.PictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.PictureBox3.Image = global::WindowsFormsApp1.Properties.Resources._006s;
+            this.PictureBox3.Location = new System.Drawing.Point(195, 375);
+            this.PictureBox3.Name = "PictureBox3";
+            this.PictureBox3.Size = new System.Drawing.Size(104, 62);
+            this.PictureBox3.TabIndex = 4;
+            this.PictureBox3.TabStop = false;
+            // 
+            // PictureBox2
+            // 
+            this.PictureBox2.Image = global::WindowsFormsApp1.Properties.Resources.shadow2;
+            this.PictureBox2.Location = new System.Drawing.Point(562, 272);
+            this.PictureBox2.Name = "PictureBox2";
+            this.PictureBox2.Size = new System.Drawing.Size(235, 166);
+            this.PictureBox2.TabIndex = 1;
+            this.PictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 192);
+            this.Controls.Add(this.PictureBox1);
+            this.Controls.Add(this.PictureBox3);
+            this.Controls.Add(this.PictureBox2);
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.ComboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Sprite Tester";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox ComboBox1;
+        private System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.PictureBox PictureBox1;
+        private System.Windows.Forms.PictureBox PictureBox2;
+        private System.Windows.Forms.Timer Timer1;
+        private System.Windows.Forms.PictureBox PictureBox3;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using WindowsFormsApp1.Properties;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private int ani;
+
+        public Form1()
+        {
+            base.Load += Form1_Load;
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ComboBox1.Text = "Down";
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "PNG Images (*.png)|*.png|All files (*.*)|*.*";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.RestoreDirectory = true;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                PictureBox3.Image = Image.FromFile(openFileDialog.FileName);
+                Timer1.Enabled = true;
+            }
+            openFileDialog.Dispose();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            Bitmap image = new Bitmap(233, 164, PixelFormat.Format32bppArgb);
+            Graphics graphics = Graphics.FromImage(image);
+            graphics.InterpolationMode = InterpolationMode.NearestNeighbor;
+            Application.DoEvents();
+            ref int reference = ref ani;
+            ref int reference2 = ref reference;
+            reference = reference2 + 1;
+            if (ani > 3)
+            {
+                ani = 0;
+            }
+            graphics.DrawImage(PictureBox2.Image, 0, 0);
+            if (PictureBox3.Image.Width > 256)
+            {
+                Rectangle destRect = new Rectangle(17, -5, 128, 128);
+                if (string.Compare(ComboBox1.Text, "Right", false) == 0)
+                {
+                    graphics.DrawImage(PictureBox3.Image, destRect, ani * 128, 256, 128, 128, GraphicsUnit.Pixel);
+                    goto graphdisposing;
+                }
+                if (string.Compare(ComboBox1.Text, "Up", false) == 0)
+                {
+                    graphics.DrawImage(PictureBox3.Image, destRect, ani * 128, 384, 128, 128, GraphicsUnit.Pixel);
+                    goto graphdisposing;
+                }
+                if (string.Compare(ComboBox1.Text, "Left", false) == 0)
+                {
+                    graphics.DrawImage(PictureBox3.Image, destRect, ani * 128, 128, 128, 128, GraphicsUnit.Pixel);
+                    goto graphdisposing;
+                }
+                graphics.DrawImage(PictureBox3.Image, destRect, ani * 128, 0, 128, 128, GraphicsUnit.Pixel);
+                goto graphdisposing;
+            }
+            Rectangle destRect2 = new Rectangle(48, 60, 64, 64);
+            if (string.Compare(ComboBox1.Text, "Right", false) == 0)
+            {
+                graphics.DrawImage(PictureBox3.Image, destRect2, ani * 64, 128, 64, 64, GraphicsUnit.Pixel);
+                goto graphdisposing;
+            }
+            if (string.Compare(ComboBox1.Text, "Up", false) == 0)
+            {
+                graphics.DrawImage(PictureBox3.Image, destRect2, ani * 64, 192, 64, 64, GraphicsUnit.Pixel);
+                goto graphdisposing;
+            }
+            if (string.Compare(ComboBox1.Text, "Left", false) == 0)
+            {
+                graphics.DrawImage(PictureBox3.Image, destRect2, ani * 64, 64, 64, 64, GraphicsUnit.Pixel);
+                goto graphdisposing;
+            }
+            graphics.DrawImage(PictureBox3.Image, destRect2, ani * 64, 0, 64, 64, GraphicsUnit.Pixel);
+            goto graphdisposing;
+            graphdisposing:
+            graphics.Dispose();
+            PictureBox1.Image = image;
+        }
+    }
+}
